f96fe1abf149357e205d5968e61b3b074675981a
eesalor <197505674+eesalor@users.noreply.github.com>
2025-03-30 13:37:59 +0300

    Add more options to recipe classes

--- init.sql
+++ init.sql
@@ -1,7 +1,13 @@
 DELETE FROM classes;
 
+INSERT INTO classes (title, value) VALUES ('Ruokalaji', 'Aamu- ja välipalat');
 INSERT INTO classes (title, value) VALUES ('Ruokalaji', 'Keitot');
 INSERT INTO classes (title, value) VALUES ('Ruokalaji', 'Salaatit');
+INSERT INTO classes (title, value) VALUES ('Ruokalaji', 'Pastat');
+INSERT INTO classes (title, value) VALUES ('Ruokalaji', 'Uuniruoat');
+INSERT INTO classes (title, value) VALUES ('Ruokalaji', 'Jälkiruoat');
 
 INSERT INTO classes (title, value) VALUES ('Erityisruokavalio', 'Gluteeniton');
+INSERT INTO classes (title, value) VALUES ('Erityisruokavalio', 'Laktoositon');
+INSERT INTO classes (title, value) VALUES ('Erityisruokavalio', 'Maidoton');
 INSERT INTO classes (title, value) VALUES ('Erityisruokavalio', 'Vegaaninen');
